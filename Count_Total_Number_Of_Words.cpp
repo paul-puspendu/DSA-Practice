a9972e57d3bd7bbc733e9a0a0137f820b720e93b
Puspendu Paul <puspendupaul81@gmail.com>
2025-07-27 00:58:48 +0600

    Add files via upload
    Counts the number of words in a given string by identifying spaces. Simple word counter using character array traversal.
    
    Topic: String

---
+++ Count_Total_Number_Of_Words.cpp
@@ -0,0 +1,22 @@
+#include <iostream>
+using namespace std;
+
+int cnt_Words(char A[])
+{
+    int c = 1;
+    for (int i = 0; A[i] != '\0'; i++)
+    {
+        if (A[i] == 32 && A[i - 1] != 32)
+        {
+            c++;
+        }
+    }
+    return c;
+}
+
+int main()
+{
+    char A[] = "How are you";
+    cout << "Total number of words is " << cnt_Words(A) << endl;
+    return 0;
+}
